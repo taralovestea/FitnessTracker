87e8ebf6a462bb46a73b48f1f4d25c29d77f235c
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-04-02 20:01:44 -0500

    heroku

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
