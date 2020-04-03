0e7570656747ca5adb070c05eba7263f9953beab
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-04-02 20:17:44 -0500

    server change

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 // const PORT = 3000;
-var PORT = process.env.port || 3000;
-
-
-
+var PORT = process.env.PORT || 3000;
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
